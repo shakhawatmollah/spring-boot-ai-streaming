ea048da0357577f23608e2f6c6cc75f61617c6c5
Shakhawat Mollah <sh.sumon99@gmail.com>
2024-10-15 18:36:49 +0600

    first commit

---
+++ src/test/java/com/shakhawat/springbootaistreaming/SpringBootAiStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakhawat.springbootaistreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAiStreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
